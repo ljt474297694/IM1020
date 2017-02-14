7bfc2b7ae1f22b2617bc231f11455803daaf8563
ljt474297694 <474297694@qq.com>
2017-02-14 21:30:46 +0800

    ContactsListFragment 联系人页面布局 视图初始化

---
+++ app/src/main/java/com/atguigu/im1020/controller/activity/AddContactActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.im1020.controller.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.atguigu.im1020.R;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
--- app/src/main/java/com/atguigu/im1020/controller/fragment/ContactsListFragment.java
+++ app/src/main/java/com/atguigu/im1020/controller/fragment/ContactsListFragment.java
@@ -1,13 +1,14 @@
 package com.atguigu.im1020.controller.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.content.Intent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.atguigu.im1020.R;
+import com.atguigu.im1020.controller.activity.AddContactActivity;
+import com.atguigu.im1020.utils.ShowToast;
+import com.hyphenate.easeui.ui.EaseContactListFragment;
 
 /**
  * Created by 李金桐 on 2017/2/14.
@@ -15,17 +16,50 @@ import android.widget.TextView;
  * 功能: xxxx
  */
 
-public class ContactsListFragment extends Fragment {
+public class ContactsListFragment extends EaseContactListFragment {
 
-    private TextView textView;
 
-    @Nullable
+    ImageView contanct_iv_invite;
+    LinearLayout ll_new_friends;
+    LinearLayout ll_groups;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        textView = new TextView(getActivity());
-        textView.setText("联系人");
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+    protected void initView() {
+        super.initView();
+        View headerView = View.inflate(getActivity(), R.layout.fragment_contact_header, null);
+        ll_new_friends = (LinearLayout) headerView.findViewById(R.id.ll_new_friends);
+        ll_groups = (LinearLayout) headerView.findViewById(R.id.ll_groups);
+        contanct_iv_invite = (ImageView) headerView.findViewById(R.id.contanct_iv_invite);
+        listView.addHeaderView(headerView);
+        // 头部图标设置
+        titleBar.setRightImageResource(R.drawable.em_add);
+        // 加号添加联系人
+    }
+
+    @Override
+    protected void setUpView() {
+        super.setUpView();
+        setListener();
+    }
+
+    private void setListener() {
+        titleBar.getRightLayout().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(), AddContactActivity.class));
+            }
+        });
+        ll_new_friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowToast.show(getActivity(),"邀请好友");
+            }
+        });
+        ll_groups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowToast.show(getActivity(),"群组");
+            }
+        });
     }
 }
