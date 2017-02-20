e2d89f298c7bb0c512072faeb0342a3311b10fb2
ljt474297694 <474297694@qq.com>
2017-02-20 16:46:34 +0800

    使用singleTask 点击按钮 解散或者退群 直接切换到GroupList页面

--- app/src/main/java/com/atguigu/im1020/controller/activity/ChatDetailsActivity.java
+++ app/src/main/java/com/atguigu/im1020/controller/activity/ChatDetailsActivity.java
@@ -1,15 +1,19 @@
 package com.atguigu.im1020.controller.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 
 import com.atguigu.im1020.R;
+import com.atguigu.im1020.utils.ShowToast;
 import com.atguigu.im1020.utils.Utils;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
+import com.hyphenate.exceptions.HyphenateException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -47,8 +51,58 @@ public class ChatDetailsActivity extends AppCompatActivity {
 
                 if(group.getOwner().equals(EMClient.getInstance().getCurrentUser())) {
                     btGroupDetail.setText("解散群");
+                    btGroupDetail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.startThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+
+                                        EMClient.getInstance().groupManager().destroyGroup(groupid);
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                ShowToast.show(ChatDetailsActivity.this,"解散成功");
+                                                startActivity(new Intent(ChatDetailsActivity.this,GroupListActivity.class));
+                                            }
+                                        });
+                                    } catch (HyphenateException e) {
+                                        e.printStackTrace();
+                                        ShowToast.showUIThread(ChatDetailsActivity.this,"解散失败");
+                                    }
+                                }
+                            });
+                        }
+                    });
                 }else {
                     btGroupDetail.setText("退群");
+
+                    btGroupDetail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Utils.startThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+
+                                        EMClient.getInstance().groupManager().leaveGroup(groupid);
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                ShowToast.show(ChatDetailsActivity.this,"退群成功");
+                                                startActivity(new Intent(ChatDetailsActivity.this,GroupListActivity.class));
+                                            }
+                                        });
+                                    } catch (HyphenateException e) {
+                                        e.printStackTrace();
+                                        ShowToast.showUIThread(ChatDetailsActivity.this,"退群失败");
+                                    }
+                                }
+                            });
+                        }
+                    });
+
                 }
             }
         });
